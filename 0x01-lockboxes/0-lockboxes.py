4014618c597208959c1a6cb6d560bb32841f1489
pelumi <polalekan@gmail.com>
2023-04-26 03:02:26 +0100

    updated task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,23 +15,20 @@ def canUnlockAll(boxes):
     """
     A method that determines if all the boxes in a list of boxes can be opened.
     """
-    opened = [boxes[0]]
+    opened_boxes = [0]
     keys = {*boxes[0]}
 
     opened_a_box = True
     while opened_a_box:
         opened_a_box = False
         for i, box in enumerate(boxes):
-            if i in keys and box not in opened:
+            if i in keys and i not in opened_boxes:
                 # add_list_to_set(box, keys)
                 for each in box:
                     keys.add(each)
-                opened.append(box)
+                opened_boxes.append(i)
                 # print('opened:', opened, end=' ')
                 # print('keys:', keys)
                 opened_a_box = True
 
-    if len(opened) == len(boxes):
-        return True
-
-    return False
+    return len(opened_boxes) == len(boxes)
