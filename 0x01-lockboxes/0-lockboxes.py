ccebafba255d2be5185d46a6be4448e56acbd694
pelumi <polalekan@gmail.com>
2023-04-27 02:28:08 +0100

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
 """
-0. Lockboxes
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially
+from 0 to n - 1 and each box may
+contain keys to the other boxes.
 """
-
-
-# def add_list_to_set(_list, _set):
-#     """
-#     Adds values in a list to a set
-#     """
-#     for each in _list:
-#         _set.add(each)
 
 
 def canUnlockAll(boxes):
     """
-    A method that determines if all the boxes in a list of boxes can be opened.
+     a method that determines if all the boxes can be opened.
+
+    :param boxes:
+    :return: True or False
     """
     if not boxes or type(boxes) is not list:
         return False
 
-    opened_boxes = {0}
-    keys = {*boxes[0]}
-
-    opened_a_box = True
-    while opened_a_box:
-        opened_a_box = False
-        for i, box in enumerate(boxes):
-            if i in keys and i not in opened_boxes:
-                # add_list_to_set(box, keys)
-                for each in box:
-                    keys.add(each)
-                opened_boxes.add(i)
-                # print('opened:', opened, end=' ')
-                # print('keys:', keys)
-                opened_a_box = True
-
-    return len(opened_boxes) == len(boxes)
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
