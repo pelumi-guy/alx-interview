e2f5904401f404aa84003198866a4b2b6843c8ec
pelumi <polalekan@gmail.com>
2023-06-01 07:11:51 +0100

    06 - solution updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,48 @@
 #!/usr/bin/node
 
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
 
 const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+// const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-const sendRequest = (characterList, index) => {
-  if (characterList.length === index) {
-    return;
-  }
+// const sendRequest = (characterList, index) => {
+//   if (characterList.length === index) {
+//     return;
+//   }
 
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
+//   request(characterList[index], (error, response, body) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(JSON.parse(body).name);
+//       sendRequest(characterList, index + 1);
+//     }
+//   });
+// };
+
+// request(movieEndpoint, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     const characterList = JSON.parse(body).characters;
+
+//     sendRequest(characterList, 0);
+//   }
+// });
+
+async function starwarsCharacters (movieId) {
+    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+    let response = await (await request(endpoint)).body;
+    response = JSON.parse(response);
+    const characters = response.characters;
+
+    for (let i = 0; i < characters.length; i++) {
+      const urlCharacter = characters[i];
+      let character = await (await request(urlCharacter)).body;
+      character = JSON.parse(character);
+      console.log(character.name);
     }
-  });
-};
-
-request(movieEndpoint, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characterList = JSON.parse(body).characters;
-
-    sendRequest(characterList, 0);
   }
-});
+
+  starwarsCharacters(movieId);
