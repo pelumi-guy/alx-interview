7593f0db6ed10026ffe42465b72edf7273a17134
pelumi <polalekan@gmail.com>
2023-06-01 07:16:59 +0100

    06 - solution updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -32,17 +32,17 @@ const movieId = process.argv[2];
 // });
 
 async function starwarsCharacters (movieId) {
-    const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-    let response = await (await request(endpoint)).body;
-    response = JSON.parse(response);
-    const characters = response.characters;
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-    for (let i = 0; i < characters.length; i++) {
-      const urlCharacter = characters[i];
-      let character = await (await request(urlCharacter)).body;
-      character = JSON.parse(character);
-      console.log(character.name);
-    }
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
+}
 
-  starwarsCharacters(movieId);
+starwarsCharacters(movieId);
