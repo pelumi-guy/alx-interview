125740b2c3684b5f858dec0efb0d0f0238614d10
pelumi <polalekan@gmail.com>
2023-06-20 02:28:41 +0100

    08 - done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+# 0. Change comes from within
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+
+    # Arrange coins in descending order
+    # total *= 100
+    coins = sorted(coins)
+    coins.reverse()
+    # print('coins:', coins)
+
+    remainder = total
+    count = 0
+
+    while remainder > 0:
+        foundCoin = False
+        for coin in coins:
+            if remainder >= coin:
+                remainder -= coin
+                count += 1
+                foundCoin = True
+                # print(f'remainder: {remainder}, coin: {coin}, count: {count}')
+                break
+        if not foundCoin:
+            return -1
+
+    return count
